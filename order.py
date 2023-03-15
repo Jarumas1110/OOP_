78a93f41e8a4601b775b7f0722118398baf60490
Natcha Bunsahwat <110528582+NatchaBunsahwat@users.noreply.github.com>
2023-03-15 16:04:11 +0700

    Update order.py

--- order.py
+++ order.py
@@ -1,8 +1,8 @@
 class Order :
  def __init__(self,product,total_price,discount,shipping_cost,net_balance,payment):
- self._product = []
- self._total_price = total_price
- self._discount = discount
- self._shipiing_cost = shipping_cost
- self._net_balance = net_balance
- self._payment = payment
+     self._product = []
+     self._total_price = total_price
+     self._discount = discount
+     self._shipiing_cost = shipping_cost
+     self._net_balance = net_balance
+     self._payment = payment
