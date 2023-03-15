8f822ec9a69527181d30e6de9a3ea158e5769f77
Jarumas1110 <88360934+Jarumas1110@users.noreply.github.com>
2023-03-15 16:02:39 +0700

    <feat>: add Adress_info

---
+++ Adress_info.py
@@ -0,0 +1,9 @@
+class Address_info :
+  def __init__(self,name,phone,address,province,district,sub_district,postcode):
+    self.name = name
+    self.phone = phone
+    self.address = address
+    self.province = province
+    self.district = district
+    self.sub_district = sub_district
+    self.postcode = postcode
