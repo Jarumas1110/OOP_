62d96f0f3f448b6a5538960ae209fc86a852c5ab
Natcha Bunsahwat <110528582+NatchaBunsahwat@users.noreply.github.com>
2023-03-15 16:05:20 +0700

    Update user_account.py

--- user_account.py
+++ user_account.py
@@ -1,7 +1,7 @@
 class User_account :
  def __int__(self,id,email,tel,gender,password):
- self.id = id 
- self.email = email
- self.tel = tel
- self.gender = gender
- self.password = password
+     self.id = id 
+     self.email = email
+     self.tel = tel
+     self.gender = gender
+     self.password = password
