2f6a9016830db1aa0f375cfdcd67d1e4e0771f36
Natcha Bunsahwat <110528582+NatchaBunsahwat@users.noreply.github.com>
2023-03-15 16:03:34 +0700

    Update customer.py

--- customer.py
+++ customer.py
@@ -1,5 +1,5 @@
 class Customer :
  def __int__(self,name_id,email,tel):
- self.name_id = name_id 
- self.email = email
- self.tel = tel
+     self.name_id = name_id 
+     self.email = email
+     self.tel = tel
