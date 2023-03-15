c719707b71abdf011bbc297dedcdc252abcd74c5
Natcha Bunsahwat <110528582+NatchaBunsahwat@users.noreply.github.com>
2023-03-15 15:25:46 +0700

    <feat>:product.py

---
+++ product.py
@@ -0,0 +1,11 @@
+class Product :
+ def __init__(self,name,sold,stock,full_price,pro_price,color,size,number,details):
+ self._name = name
+ self._sold = sold
+ self._stock = stock
+ self._full_price = full_price
+ self._pro_price = pro_price
+ self._color = color
+ self._size = size
+ self._number = number
+ self._details = details
